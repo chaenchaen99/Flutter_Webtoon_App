ef1e7b3d49eb1a3d13cd0ef7a3b163bbe913884e
chaeyeon <chaenjung.99@gmail.com>
2024-04-18 22:28:48 +0900

    BuildContext theme 적용

--- flutter_biginner/lib/stateful/stateful_main.dart
+++ flutter_biginner/lib/stateful/stateful_main.dart
@@ -12,38 +12,39 @@ class StatefulMain extends StatefulWidget {
 }
 
 class _StatefulMainState extends State<StatefulMain> {
-  //Dart 클래스 프로퍼티
-  List<int> numbers = [];
-
-  void onClick() {
-    setState(() {
-      //State클래스에게 데이터가 변경되었다고 알리는 함수
-      //아래 있는 build method를 다시 실행시킴
-      numbers.add(numbers.length);
-    });
-
-    numbers.add(numbers.length);
-    print(numbers.length);
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
+      home: const Scaffold(
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text('Click Count'),
-              for (var n in numbers) Text('$n'),
-              TextButton(
-                onPressed: onClick,
-                child: const Text('+'),
-              ),
+              MyLargeTitle(),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+          fontSize: 30, color: Theme.of(context).textTheme.titleLarge?.color),
     );
   }
 }
