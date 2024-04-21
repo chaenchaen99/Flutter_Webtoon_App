02cb0fd78e4802d176e5981837d09b67536360f2
chaeyeon <chaenjung.99@gmail.com>
2024-04-21 19:22:16 +0900

    widget lifecycle 공부

--- flutter_biginner/lib/stateful/stateful_main.dart
+++ flutter_biginner/lib/stateful/stateful_main.dart
@@ -12,8 +12,18 @@ class StatefulMain extends StatefulWidget {
 }
 
 class _StatefulMainState extends State<StatefulMain> {
+  bool showTitle = true;
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    //BuildContext를 통해서 아주 먼 곳의 요소에 접근할 수 있다.
+    //위젯트리에서 위젯의 위치를 제공하고, 이를 통해 상위 요소 데이터에 접근할 수 있다.
     return MaterialApp(
       theme: ThemeData(
         textTheme: const TextTheme(
@@ -22,12 +32,16 @@ class _StatefulMainState extends State<StatefulMain> {
           ),
         ),
       ),
-      home: const Scaffold(
+      home: Scaffold(
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              MyLargeTitle(),
+              showTitle ? const MyLargeTitle() : const Text('notiong'),
+              IconButton(
+                onPressed: toggleTitle,
+                icon: const Icon(Icons.remove_red_eye),
+              ),
             ],
           ),
         ),
@@ -36,8 +50,26 @@ class _StatefulMainState extends State<StatefulMain> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({super.key});
+
+  @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  int count = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    //위젯이 스크린에서 제거될 때 호출
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
